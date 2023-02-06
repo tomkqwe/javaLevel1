285cea3edf65b8b7672f36e50e10a1821bb83b6f
avlebedev <avlebedev@fil-it.ru>
2023-02-06 14:39:52 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String s = "World!";
+        String hello = "Hello";
+        System.out.println(hello + " " + s);
+    }
+}
